3f4b5fb16376b047e20296b43a01aa586f7ce17c
Jon Reiling <jon.reiling@akqa.com>
2015-12-22 09:55:29 -0500

    Added API for album art and addition volume options

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server, {"log":true});
+var request = require('request');
 
 var currentTrack;
 var currentVolume;
@@ -15,6 +16,10 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
+
+app.get('/', function(req, res){
+  res.send({'connected':connectedToPlayer});
 });
 
 app.get('/status', function(req, res){
@@ -31,13 +36,58 @@ app.get('/', function(req, res){
 
 app.get('/api/pause', function(req, res){
 	io.of('/rockbox-player').emit('pause');
-	res.send('');
+	res.send({'success':connectedToPlayer});
 });
 
 app.get('/api/skip', function(req, res){
 	io.of('/rockbox-player').emit('skip');
-	res.send('');
+	res.send({'success':connectedToPlayer});
 });
+
+app.get('/api/volume/up', function(req, res){
+	io.of('/rockbox-player').emit('setVolume','up');
+	res.send({'success':connectedToPlayer});
+});
+
+app.get('/api/volume/down', function(req, res){
+	io.of('/rockbox-player').emit('setVolume','down');
+	res.send({'success':connectedToPlayer});
+});
+
+app.get('/api/volume/normal', function(req, res){
+	io.of('/rockbox-player').emit('setVolume','normal');
+	res.send({'success':connectedToPlayer});
+});
+
+app.get('/api/volume/low', function(req, res){
+	io.of('/rockbox-player').emit('setVolume','low');
+	res.send({'success':connectedToPlayer});
+});
+
+app.get('/images/:album', function(req, res){
+	var id = req.params.album;
+	id = id.replace("spotify:album:","");
+
+
+	request( 'https://api.spotify.com/v1/albums/' + id , function (error, response, body) {
+
+		if (!error){
+
+			var json = JSON.parse( body );
+			console.log(json.images[0].url);
+
+			res.writeHead(302, {location:json.images[0].url});
+			res.end();
+
+		} else {
+			res.send('');
+		}
+	});
+
+	console.log(id);
+
+});
+
 
 var playerSocket = io
 	.of('rockbox-player')
@@ -45,6 +95,7 @@ var playerSocket = io
 		
 		console.log( "Player connected" );
 		connectedToPlayer = true;
+		io.of('/rockbox-client').emit('connectionUpdate',true);
 
 		socket.on('trackUpdate',function(data) {
 			currentTrack = data;
@@ -64,6 +115,7 @@ var playerSocket = io
 		socket.on('disconnect',function() {
 			currentTrack = null;
 			io.of('/rockbox-client').emit('trackUpdate',null);
+			io.of('/rockbox-client').emit('connectionUpdate',false);
 			connectedToPlayer = false;
 		});
 
