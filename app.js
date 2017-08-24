9657f446601c2d0f7591d67dcedd2837735ba8ff
Jon Reiling <jon.reiling@akqa.com>
2017-08-24 17:29:27 -0400

    Updated allowed methods

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ routerApi.use(function(req, res, next) {
 	console.log('check');
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   next();
 });
 
